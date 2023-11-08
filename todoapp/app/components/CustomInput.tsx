83c23761558955a03e6015adad52f0f7599d2674
MacBook Pro <macbookpro@Elijah-Owusu-Ewooral-Boahen.local>
2023-11-08 16:57:55 +0000

    feat: Create a todo app

--- todoapp/app/components/CustomInput.tsx
+++ todoapp/app/components/CustomInput.tsx
@@ -1,10 +1,18 @@
 import styles from '@/styles/custom.module.scss'
+import {useState} from "react";
 const CustomInput = () => {
+    // const [inputValues, setInputValues] = useState('')
     return (
         <div className={styles['Custom__Wrapper']}>
             <div className={styles['Custom__Input']}>
-                <label htmlFor="todo">Itinerary:</label>
-                <input type="text" placeholder='Enter what you wanna do' />
+                <header>
+                    A TODO APP
+                </header>
+                <form action="">
+                    <label htmlFor="todo">Itinerary:</label>
+                    <input type="text" placeholder='Enter what you wanna do' />
+                    <button type='submit'>send</button>
+                </form>
             </div>
         </div>
     )
