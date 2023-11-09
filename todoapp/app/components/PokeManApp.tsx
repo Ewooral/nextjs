c5f3a04ffe60117e1d3dec1b96e3cf154d1fc818
MacBook Pro <macbookpro@Elijah-Owusu-Ewooral-Boahen.local>
2023-11-09 16:49:42 +0000

    feat: Create a pokeman search app

--- todoapp/app/components/PokeManApp.tsx
+++ todoapp/app/components/PokeManApp.tsx
@@ -1,3 +1,5 @@
+import PokeManStyle from "@/styles/style.module.css"
+import pokemon from "@/data/pokeman.json"
 const PokeManApp = () => {
     return (
         <div
@@ -7,6 +9,24 @@ const PokeManApp = () => {
                 paddingTop: "1rem"
             }}
         >
+            <h1 className={PokeManStyle["title"]}>Pokemon Search</h1>
+            <table width="100%">
+                <thead>
+                <tr className={PokeManStyle["PokeMan"]}>
+                    <th>Name</th>
+                    <th>Type</th>
+                </tr>
+                </thead>
+                <tbody>
+                {pokemon.map((pokemon, id) => (
+                    <tr key={id}>
+                        <td>{pokemon.name.english}</td>
+                        <td>{pokemon.type.join(", s")}</td>
+                    </tr>
+                ))}
+
+                </tbody>
+            </table>
 
         </div>
     )
--- todoapp/app/page.tsx
+++ todoapp/app/page.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import CustomInput from "@/app/components/TodoApp";
 import CustomTodo from "@/app/components/CustomTodo"
 import CustomToggle from "@/app/components/CustomToggle";
+import PokeManApp from "@/app/components/PokeManApp";
 export default function Home() {
     return (
         <div>
             <CustomInput/>
             <CustomTodo />
             <CustomToggle />
+            <PokeManApp />
         </div>
     )
 }
